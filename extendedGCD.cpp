41cd0c2cfa25e35da6e225e113bd54b54ab1550f
hemant2132 <45938556+hemant2132@users.noreply.github.com>
2020-02-28 20:00:57 +0530

    Extended GCD + Chinese Remainder Theorem
    tested code

--- extendedGCD.cpp
+++ extendedGCD.cpp
@@ -1,12 +1,41 @@
-int gcd(int a, int b, int & x, int & y) {
-    if (a == 0) {
-        x = 0;
-        y = 1;
-        return b;
+struct gcd_type
+{
+    int x,y,d;
+};
+
+gcd_type exgcd(int a,int b)
+{
+    if(b==0)
+        return {1, 0, a};
+
+    gcd_type tmp=exgcd(b,a%b);
+    return {tmp.y, tmp.x-(a/b)*tmp.y, tmp.d};
+}
+
+inline int take_mod(int x, int mod) { x%=mod; if(x<0) x+=mod; return x;}
+
+int n=2;
+vector<int> rem(n),num(n);
+
+void CRT()
+{
+    int res=rem[0];
+    int l=num[0];
+    for(int i=1;i<n;++i)
+    {
+        auto tmp=exgcd(l,num[i]);
+        int x1=tmp.x;
+        int d=tmp.d;
+
+        if((rem[i]-res)%d)
+        {
+            cout<<"no solution";
+            return;
+        }
+
+        res=take_mod( res + (x1*((rem[i]-res)/d))%(num[i]/d) * l , l*(num[i]/d) );
+        l=l*(num[i]/d);
     }
-    int x1, y1;
-    int d = gcd(b % a, a, x1, y1);
-    x = y1 - (b / a) * x1;
-    y = x1;
-    return d;
+
+    cout<<res<<" "<<l;
 }
